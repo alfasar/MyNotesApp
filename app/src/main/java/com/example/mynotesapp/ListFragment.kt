10d2ba5dc6af967b4238641d9107075ac482dcc7
alfasar <amettyristhirhara@gmail.com>
2022-04-26 18:33:53 +0300

    Настройка взаимодействия фрагментов

--- app/src/main/java/com/example/mynotesapp/ListFragment.kt
+++ app/src/main/java/com/example/mynotesapp/ListFragment.kt
@@ -1,7 +1,32 @@
 package com.example.mynotesapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.mynotesapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment(R.layout.fragment_list) {
 
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_newItemFragment)
+        }
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/mynotesapp/LoginFragment.kt
+++ app/src/main/java/com/example/mynotesapp/LoginFragment.kt
@@ -1,7 +1,36 @@
 package com.example.mynotesapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.mynotesapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.loginButton.setOnClickListener {
+            //inputCheck
+            findNavController().navigate(R.id.action_loginFragment_to_listFragment)
+        }
+        binding.regButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_regFragment)
+        }
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
